f87b6fb4d825f2b8e5eea8b33122b2ee0c9da5c7
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-13 19:22:39 +0700

    Stateless Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[400],
-    ),
-    body: Center(
-      child: Text(
-        'hello taufan',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      backgroundColor: Colors.red[400],
-      child: Text('click'),
-    ),
-  )
+  home: Home()
 ));
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[400],
+      ),
+      body: Center(
+        child: Text(
+          'hello taufan',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[400],
+        child: Text('click'),
+      ),
+    );
+  }
+}
